b4642a4aed590e5e6edfc2d2a213d6c6330f7842
CuauMa <temcmm99@hotmail.com>
2021-09-24 00:50:56 -0700

    Adding a new service.

--- src/main/java/com/myorg/MyWidgetServiceStack.java
+++ src/main/java/com/myorg/MyWidgetServiceStack.java
@@ -12,6 +12,6 @@ public class MyWidgetServiceStack extends Stack {
     public MyWidgetServiceStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
+        new WidgetService(this, "Widgets");
     }
 }
